e0046d95cf8ecad6aeaee5ab093f5c3cf9229636
Issa DEMIRCI <issa.demirci@epitech.eu>
2021-02-03 01:37:11 +0100

    devis personnalisé

--- js/simulateur.js
+++ js/simulateur.js
@@ -1,13 +1,55 @@
-let togg2 = document.getElementById("togg2");
-let d2 = document.getElementById("devisPerso");
+let togg1 = document.getElementById("btnDevisSimu")
+let togg2 = document.getElementById("btnDevisPerso");
 
+let d1 = document.getElementById("simulateur")
+let d2 = document.getElementById("devisPerso") 
 
-function togg() {
-    if(getComputedStyle(d2).display != "none") {
+togg1.addEventListener("click", () => {
+    if(getComputedStyle(d1).display != "block") {
+        d1.style.display = "block";
         d2.style.display = "none";
-    } else {
-        d2.style.display = "block";
-    }
-};
+    } 
+})
 
-togg2.onclick = togg;
+togg2.addEventListener("click", () => {
+    if(getComputedStyle(d2).display != "block") {
+        d2.style.display = "block";
+        d1.style.display = "none";
+
+    }
+})
+
+
+
+// function devisSim() {
+//     // let d1 = document.getElementById("simulateur"); 
+//     if(d1.style.display != "block") {
+//         d1.style.display = "block";
+//         d2.style.display = "none";
+//     } 
+// }
+
+// function devisPerson() {
+//     // let d2 = document.getElementById("devisPerso");
+//     if(d2.style.display != "block") {
+//         d2.style.display = "block";
+//         d1.style.display = "none";
+//     }
+// }
+
+
+
+
+// function toggle1() {
+//     if(getComputedStyle(d2).display != "block") {
+//         d2.style.display = "block";
+//         d1.style.display = "none";
+//         console.log("ok");
+//     } else {
+//         d2.style.display = "none";
+//     }
+// };
+
+// togg2.onclick = toggle1;
+
+
