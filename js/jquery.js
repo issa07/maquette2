dfd0aa74c7d49a56a1ecef5187790370bdcc50a5
Issa DEMIRCI <issa.demirci@epitech.eu>
2021-02-04 18:30:44 +0100

    form-devis

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
     $(this).on("click", ".add", function() {
-        let html = ' <div class="form-group"> <select name="maçonnerie" id="maconnerie"> <optgroup label="Maçonnerie"> <option value="maçonnerieMur">Maçonnerie murs</option> <option value="maçonnerieExterieur">Maçonnerie extérieur</option> </optgroup> </select> </div>'
-        
+        let html = `<div class="form-group">
+        <select name="maçonnerie" id="maconnerie">
+          <optgroup label="Maçonnerie">
+            <option value="maçonnerieMur">Maçonnerie murs </option>
+            <option value="maçonnerieExterieur">Maçonnerie extérieur</option>
+          </optgroup>
+          <optgroup label="Platrerie isolation">
+            <option value="doublagePlatreCloison">doublage Platre et Cloison</option>
+            <option value="plafond">Plafond</option>
+          </optgroup>
+          <optgroup label="Rêvetement mur/plafond">
+            <option value="PeintureEtEnduits">Peinture et Enduits </option>
+            <option value="AutresRêvetemens">Autres rêvetemens</option>
+          </optgroup>
+        </select>
+        <i class="fas fa-times"></i>
+      </div>`        
         $(".select-div").append(html);
     })
 })
