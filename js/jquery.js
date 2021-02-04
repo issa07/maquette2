504df3370a787c22338fb94cbf71396eea60cc55
Issa DEMIRCI <issa.demirci@epitech.eu>
2021-02-04 19:19:47 +0100

    form-devis

--- js/jquery.js
+++ js/jquery.js
@@ -5,18 +5,26 @@ $(document).ready(function() {
           <optgroup label="Maçonnerie">
             <option value="maçonnerieMur">Maçonnerie murs </option>
             <option value="maçonnerieExterieur">Maçonnerie extérieur</option>
+            <option value="constructionMaison">Construction maison</option>
           </optgroup>
           <optgroup label="Platrerie isolation">
             <option value="doublagePlatreCloison">doublage Platre et Cloison</option>
             <option value="plafond">Plafond</option>
           </optgroup>
-          <optgroup label="Rêvetement mur/plafond">
-            <option value="PeintureEtEnduits">Peinture et Enduits </option>
-            <option value="AutresRêvetemens">Autres rêvetemens</option>
+          <optgroup label="Rêvetement sol/mur/plafond">
+            <option value="peintureEtEnduits">Peinture mur, plafond, boiseries </option>
+            <option value="carlageSol">Carlage sol</option>
+            <option value="parquet">Parquet (neuf ou rénovation)</option>
+            <option value="autresRêvetemens">Autres rêvetemens</option>
+          </optgroup>
+          <optgroup label="Extérieur & Toitures">
+            <option value="carlageExterieur">Carlage Extérieur </option>
+            <option value="nettoyageFacade">Nettoyage de façade</option>
           </optgroup>
         </select>
         <i class="fas fa-times"></i>
-      </div>`        
+      </div>`   
+
         $(".select-div").append(html);
     })
 })
