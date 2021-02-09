0c6c92b392ffab2bacede96eeacef0d885f49e12
Issa DEMIRCI <issa.demirci@epitech.eu>
2021-02-09 16:57:40 +0100

    form-devis

--- js/simulateur.js
+++ js/simulateur.js
@@ -4,9 +4,9 @@ let togg2 = document.getElementById("btnDevisPerso");
 let d1 = document.getElementById("overlay")
 let d2 = document.getElementById("devisPerso") 
 
-togg1.addEventListener("click", () => {
+// togg1.addEventListener("click", () => {
     
-})
+// })
 
 // togg2.addEventListener("click", () => {
 //     if(getComputedStyle(d2).display != "block") {
@@ -25,4 +25,69 @@ document.getElementById("btnDevisSimu").addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = "none";
 });
+
+
+
+
+
+// POUR FORMULAIRE 
+// Etape 1
+const step1 = document.getElementById('construct')
+const step2 = document.getElementById('extension')
+const step3 = document.getElementById('surelevation')
+const step4 = document.getElementById('renovation')
+const step5 = document.getElementById('r&e')
+
+// Etape Une "projet"
+function etapeUne(detail) {
+    // Data-choice[a-e]
+    let dataChoice = detail.getAttribute("data-choice")
+
+    document.querySelector('.etape-2').style.display = "block";
+    document.querySelector('#etape-1').style.display = "none";
+
+    console.log(dataChoice)
+    console.log(detail.innerHTML)
+}
+
+// Etape deux "type"
+function etapeDeux(detail) {
+    console.log(detail.innerHTML)
+
+    document.querySelector('.etape-2').style.display = "none";
+    document.querySelector('#etape-3').style.display = "block";
+}
+
+// Etape 3 "surface"
+function etapeTrois() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("construction").addEventListener('click', function() {
+//     document.querySelector('.testing').style.display = "block";
+//     document.querySelector('#step-1').style.display = "none";
+// })
+
+// document.getElementById("maison").addEventListener('click', function() {
+//     document.querySelector('.surface').style.display = "block";
+// })
+
+
+
+
+
+
 
